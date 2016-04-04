1d771498ecedc11c547add54cda25638db4dabfb
Nick Papatonis <npapatonis@guard1.com>
2016-04-04 09:19:55 -0400

    initial version

---
+++ ngTest04/App/app.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    "use strict";
+
+    angular.module('ngTest04', ['ui.router', 'ngAnimate', 'ui.bootstrap'])
+        .config(config);
+
+    function config($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                template: 'Select a menu item.'
+            })
+            .state('testAreaOne', {
+                url: '/test/area/one',
+                templateUrl: 'App/Views/TestArea01/test.html'
+            })
+            .state('testAreaTwo', {
+                url: '/test/area/two',
+                templateUrl: 'App/Views/TestArea02/test.html'
+            });
+
+    };
+
+})();
